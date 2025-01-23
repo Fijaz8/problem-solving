d64d7e92ce9f9e240a0651993d1826c8f7d6f03d
Fijaz8 <123264875+Fijaz8@users.noreply.github.com>
2025-01-23 21:13:02 +0530

    commiting the problem reverse of number and inlcude gitignore

---
+++ reverse_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int num = 45623,reversed=0;
+    while (num>0)
+    {
+        /* code */
+        int temp= num%10;
+        reversed = reversed*10+temp;
+        num = num/10;
+
+    }
+    printf("%d", reversed);
+    
+}
